cf6ea512287b79f7c8f993cc13e3aeb86ac32000
Gustavo Floriano <160735371+gustavofloriano11@users.noreply.github.com>
2024-06-27 19:25:35 -0300

    Add files via upload

---
+++ front-end/scripts/mudarPagina.js
@@ -0,0 +1,7 @@
+function site_login(){
+    window.location.replace("front-end/estrutura/login.html")
+};
+
+function site_cadastro(){
+    window.location.replace("front-end/estrutura/cadastro.html")
+}
